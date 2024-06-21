753b563ccb62c0b335e32892d18b70350db3e9d7
Meheni Samson <54425905+mehens1@users.noreply.github.com>
2024-06-21 20:09:19 +0100

    bug fix on the fourth task

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,6 +1,7 @@
 hours = 2
-seconds_in_an_hour = 3600
+conversion_factor = 60 * 60  # 3600 seconds in an hour
 
-seconds = hours * seconds_in_an_hour
+seconds = hours
+seconds *= conversion_factor
 
 print(f"{hours} hour(s) is {seconds} seconds")
